8d8e1ba3f1ea9f5c62269e010663a6ed4c31c7d0
chen <2813204231@qq.com>
2017-06-21 23:12:38 +0800

    首次提交-0

---
+++ JavaSETest/src/com/javase/networkCommunication/InetAddressTest.java
@@ -0,0 +1,26 @@
+package com.javase.networkCommunication;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.junit.Test;
+
+/**
+ * InetAddress是一个从IP角度反映网络上计算机的对象
+ * 
+ * @author tonghuo
+ *
+ */
+public class InetAddressTest {
+
+	@Test
+	public void test01() throws UnknownHostException {
+		InetAddress localHost = InetAddress.getLocalHost();
+		byte[] address = localHost.getAddress();
+		for (byte x : address) {
+			System.out.println("x=" + x);
+		}
+		System.out.println("hostName=" + localHost.getHostName());
+	}
+
+}
