443a80dda68787d704dec803bbb0f318d90be31c
chen <2813204231@qq.com>
2017-06-25 11:08:17 +0800

    修改注释

--- JavaSETest/src/com/javase/classtest/FinalClassTest.java
+++ JavaSETest/src/com/javase/classtest/FinalClassTest.java
@@ -1,16 +1,27 @@
 package com.javase.classtest;
 
 /**
+ * 1.一个变量被定义为final类型，如果该变量指向基本数据类型，代表的字面值不可变；
+ * 如果指向域模型，那么指向的对象不可变，对象的属性可以变。
+ * 2.全局变量被定义为final类型，必须在定义时初始化或者在构造器中初始化。
+ * 3.局部变量被定义为final类型，必须在定义时初始化。
  * 
  * @author tonghuo
- * 
- *         1.һ���������͵ı���������Ϊfinal���ͣ���ô�����������������ʱ�͸�ֵ������ֻ�ܸ�ֵһ�Σ�
- * 
- *         2.һ���������͵ı���������Ϊfinal���ͣ���ô�����������������ʱ��ָ�����󣬲�����ָ��Ķ����ܸı䣬 ��������ݿ��Ըı䡣
+ *
  */
-
 public class FinalClassTest {
 
+	private final User user02;
+	private final int a01;
+
+	public FinalClassTest(User user02, int a01) {
+		this.user02 = user02;
+		this.a01 = a01;
+	}
+
+	/* public void init(User user){
+	   this.user02=user;
+	 }*/
 	@SuppressWarnings("unused")
 	private static final int a = 3;
 
